2758421328916113d164a728bd0d29393f1ee80b
clara <clara@clara.com>
2016-08-29 14:41:17 -0500

    Toast time message working!

--- app/src/main/java/com/clara/myapplication/MainActivity.java
+++ app/src/main/java/com/clara/myapplication/MainActivity.java
@@ -3,6 +3,11 @@ package com.clara.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		Log.d("HELLO", "This is a test");
+
+		Button timeButton = (Button) findViewById(R.id.button);
+		timeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String currentTime = new Date().toString();
+				Toast.makeText(MainActivity.this, "The time is " + currentTime, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 }
